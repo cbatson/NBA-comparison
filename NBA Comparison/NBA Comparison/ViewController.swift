198f1922a3794037ecb2ec9b6fdbce21cd404861
alexzhou860 <52668114+alexzhou860@users.noreply.github.com>
2020-07-09 14:36:35 -0400

    Update ViewController.swift

--- NBA Comparison/NBA Comparison/ViewController.swift
+++ NBA Comparison/NBA Comparison/ViewController.swift
@@ -10,38 +10,42 @@ import UIKit
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
     
     var players = [Datum]()
+    var metadata = Meta(totalPages: 0, currentPage: 0, nextPage: 0, perPage: 0, totalCount: 0)
     var playerOneSearches = [Datum]()
-    var playerTwoSearches = [Datum]()
+    var url = URL(string: "https://www.balldontlie.io/api/v1/players?page=60")
+    //var pageNumber = 2
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        getData()
-        playerOneSearchBar.delegate = self
-        playerTwoSearchBar.delegate = self
+        getData(url: url!)
+    }
+    
+    func changePage() {
+        print(metadata.currentPage)
+        if (metadata.nextPage != TID_NULL) {
+            let url = URL(string: "https://www.balldontlie.io/api/v1/players?page=\(metadata.nextPage)")
+            getData(url: url!)
+        }
     }
     
     @IBOutlet weak var playerOneSearchBar: UISearchBar!
     @IBOutlet weak var playerOneTableView: UITableView!
-    @IBOutlet weak var playerTwoSearchBar: UISearchBar!
-    @IBOutlet weak var playerTwoTableView: UITableView!
 
-    func getData() {
-        
-        let url = URL(string: "https://www.balldontlie.io/api/v1/players")
-        
-        URLSession.shared.dataTask(with: url!) { data,reponse,error in
+    func getData(url:URL) {
+        URLSession.shared.dataTask(with: url) { data,reponse,error in
                 if error==nil {
                     do {
                         let downloadedPlayers = try JSONDecoder().decode(Response.self, from: data!)
-                        self.players = downloadedPlayers.data
-                        self.playerOneSearches = downloadedPlayers.data
+                        self.players += downloadedPlayers.data
+                        self.metadata = downloadedPlayers.meta
+                        self.playerOneSearches = self.players
                         DispatchQueue.main.async {
                             self.playerOneTableView.reloadData()
-                            self.playerTwoTableView.reloadData()
+                            self.changePage()
                         }
                         } catch{
-                            print("Json Error")
+                            print("Json Error : \(error)")
                         }
                     }
         }.resume()
@@ -72,99 +76,4 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         }
         self.playerOneTableView.reloadData()
     }
-
-
-        //define data format so that json can match
-// MARK: - Response
-struct Response: Codable {
-    let data: [Datum]
-    let meta: Meta
-}
-
-// MARK: - Datum
-struct Datum: Codable {
-    let id: Int
-    let firstName: String
-    let heightFeet, heightInches: Int?
-    let lastName, position: String
-    let team: Team
-    let weightPounds: Int?
-
-    enum CodingKeys: String, CodingKey {
-        case id
-        case firstName = "first_name"
-        case heightFeet = "height_feet"
-        case heightInches = "height_inches"
-        case lastName = "last_name"
-        case position, team
-        case weightPounds = "weight_pounds"
-    }
-}
-
-// MARK: - Team
-struct Team: Codable {
-    let id: Int
-    let abbreviation, city: String
-    let conference: Conference
-    let division, fullName, name: String
-
-    enum CodingKeys: String, CodingKey {
-        case id, abbreviation, city, conference, division
-        case fullName = "full_name"
-        case name
-    }
-}
-
-enum Conference: String, Codable {
-    case east = "East"
-    case west = "West"
-}
-
-// MARK: - Meta
-struct Meta: Codable {
-    let totalPages, currentPage, nextPage, perPage: Int
-    let totalCount: Int
-
-    enum CodingKeys: String, CodingKey {
-        case totalPages = "total_pages"
-        case currentPage = "current_page"
-        case nextPage = "next_page"
-        case perPage = "per_page"
-        case totalCount = "total_count"
-    }
-}
-/*struct Response: Codable{
-    var response: Data
-}
-struct Data: Codable{
-    var data: [PlayerInfo]
-    var meta: MetaInfo
-}
-struct PlayerInfo: Codable{
-    var id: Int
-    var first_name: String
-    var height_feet: Int?
-    var height_inches: Int?
-    var last_name: String
-    var position: String
-    var team: TeamInfo
-    var weight_pounds: Int?
-}
-struct TeamInfo: Codable{
-    var id: Int
-    var abbreviation: String
-    var city: String
-    var conference: String
-    var division: String
-    var full_name: String
-    var name: String
-}
-struct MetaInfo: Codable{
-    var total_pages: Int
-    var current_page: Int
-    var next_page: Int
-    var per_page: Int
-    var total_count: Int
-}
-*/
 }
