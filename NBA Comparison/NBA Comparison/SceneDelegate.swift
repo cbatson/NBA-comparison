4fdba0aaae0c67953561fa4782799003c2d6ad2e
Chuck Batson <cbatson3@gmail.com>
2020-07-25 11:44:09 -0700

    re-implement in SwiftUI

--- NBA Comparison/NBA Comparison/SceneDelegate.swift
+++ NBA Comparison/NBA Comparison/SceneDelegate.swift
@@ -2,11 +2,12 @@
 //  SceneDelegate.swift
 //  NBA Comparison
 //
-//  Created by Owner on 4/10/20.
-//  Copyright © 2020 AlexZhou. All rights reserved.
+//  Created by chuck on 7/24/20.
+//  Copyright © 2020 AlexZ. All rights reserved.
 //
 
 import UIKit
+import SwiftUI
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
@@ -17,7 +18,17 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        // Create the SwiftUI view that provides the window contents.
+        let contentView = ContentView()
+
+        // Use a UIHostingController as window root view controller.
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = UIHostingController(rootView: contentView)
+            self.window = window
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -51,3 +62,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+
+struct SceneDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
