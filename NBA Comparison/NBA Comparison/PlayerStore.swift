a8a4e7836db4fb0dafe97f80db7f22d51249e934
Chuck Batson <cbatson3@gmail.com>
2020-08-05 13:04:51 -0700

    don't add duplicate players

--- NBA Comparison/NBA Comparison/PlayerStore.swift
+++ NBA Comparison/NBA Comparison/PlayerStore.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 class PlayerStore : ObservableObject {
     @Published var players: [Player] = []
+    private var playerIds = Set<Int>()
 
     init(initPlayers : [Player] = []) {
         players = initPlayers
@@ -19,10 +20,17 @@ class PlayerStore : ObservableObject {
         players = []
     }
     
-    func appendWithSort(players: [Player]) {
+    func appendWithSort(players: [Player]) -> Int {
+        // Include only those players who are new to the playerIds set.
+        let filteredPlayers = players.filter { !playerIds.contains($0.remoteId) }
+        // Update the playerIds set to include the new players.
+        playerIds.formUnion(Set(players.map { $0.remoteId }))
+        // Combine the existing player with the new players and sort them.
+        let newPlayers = (self.players + filteredPlayers).sorted(by: { $0.sortName < $1.sortName })
         DispatchQueue.main.async {
             // Publishing needs to happen on the main thread.
-            self.players = (self.players + players).sorted(by: { $0.sortName < $1.sortName })
+            self.players = newPlayers
         }
+        return newPlayers.count
     }
 }
