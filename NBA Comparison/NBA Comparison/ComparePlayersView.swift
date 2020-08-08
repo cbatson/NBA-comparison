a17c379fb84ed2ebf2c148d45089050ae1b7b52c
Chuck Batson <cbatson@users.noreply.github.com>
2020-08-08 11:26:09 -0700

    tab view and about screen (#10)
    * don't add duplicate players
    
    * added a tab view and about page

---
+++ NBA Comparison/NBA Comparison/ComparePlayersView.swift
@@ -0,0 +1,23 @@
+//
+//  ComparePlayersView.swift
+//  NBA Comparison
+//
+//  Created by chuck on 8/5/20.
+//  Copyright © 2020 AlexZ. All rights reserved.
+//
+
+import SwiftUI
+
+struct ComparePlayersView: View {
+    var body: some View {
+        NavigationView {
+            SelectPlayerView()
+        }
+    }
+}
+
+struct ComparePlayersView_Previews: PreviewProvider {
+    static var previews: some View {
+        ComparePlayersView()
+    }
+}
